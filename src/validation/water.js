2fe8c08d578f1c8fd63cc108072afbaad60aaf97
Marianna Honcharuk <148065963+MariannaHon@users.noreply.github.com>
2024-08-30 09:31:26 +0200

    Update water.js

--- src/validation/water.js
+++ src/validation/water.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const waterNotes = Joi.object({
-    waterVolume: Joi.number().min(1).max(5000).required(),
-    date: Joi.date().format('YYYY-MM-DD HH:mm'),
+    amount: Joi.number().min(2).max(7).required(),
+    time: Joi.date().format('YYYY-MM-DD HH:mm'),
 });
