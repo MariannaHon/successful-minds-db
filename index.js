2d16291c196ce5fafb29813a00b10abe8cf6d3d3
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-08-31 00:19:20 +0300

    add create dirs for avatars

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import { initMongoConnection } from "./src/db/initMongoConnection.js";
-import { setupServer } from "./server.js";
-
+import { initMongoConnection } from './src/db/initMongoConnection.js';
+import { setupServer } from './server.js';
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TMP_UPLOAD_DIR, UPLOAD_AVATAR_DIR } from './constants/index.js';
 
 const bootstrap = async () => {
-    await initMongoConnection();
-    setupServer();
+  await initMongoConnection();
+  await createDirIfNotExists(TMP_UPLOAD_DIR);
+  await createDirIfNotExists(UPLOAD_AVATAR_DIR);
+  setupServer();
 };
 
 void bootstrap();
-
