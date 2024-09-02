628261099d6cdfb08d5370aed561507ebe61b6eb
Marianna Honcharuk <148065963+MariannaHon@users.noreply.github.com>
2024-09-02 19:40:12 +0200

    Update auth.js

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -4,7 +4,6 @@ import Joi from 'joi';
 export const signupUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(8).max(64).required(),
-  repeatPassword: Joi.string().min(8).max(64).required(),
 });
 
 export const signinUserSchema = Joi.object({
